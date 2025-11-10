4bfdf51829cb572ab627404ef4d34eb3b5be908f
Ntsoaki <ntsoakisoai@gmail.com>
2025-11-11 00:02:11 +0200

    "Added updates to admin"

--- sabuzz/admin.py
+++ sabuzz/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import Post, Category, Comment, Profile
+from .models import Post, Category, Comment, Profile, Like, Subscriber
 
 # Register your models here.
+admin.site.register(Profile)
 admin.site.register(Category)
-
-class CatergoryAdmin(admin.ModelAdmin):
-list_display= ('name')
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.site.register(Profile)
+admin.site.register(Like)
+admin.site.register(Subscriber)
