bcfb309aea1fe109a2319e81b2dfe7d3f4747b15
Ntsoaki <ntsoakisoai@gmail.com>
2025-11-11 01:08:08 +0200

    Resolve merge conflict in admin.py by manually resolving the conflict

--- sabuzz/admin.py
+++ sabuzz/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import Post, Category, Comment
-
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
-=======
 from .models import Post, Category, Comment, Profile, Like, Subscriber
 
 # Register your models here.
@@ -16,4 +8,4 @@ admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(Subscriber)
->>>>>>> dev-backend
+
