2f4527e101a6eaace3d54259f6f1c238155ae35e
Ntsoaki <ntsoakisoai@gmail.com>
2025-11-24 09:39:57 +0200

    Modified model and added stripe

--- sabuzz/models.py
+++ sabuzz/models.py
@@ -6,8 +6,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     ROLE_CHOICE = [
         ('admin', 'Admin'),
-        ('journalist', 'Journalist'),
-        ('publisher', 'Publisher'),
+        ("subscriber", "Subscriber"),
         ('user', 'User'),
     ]
     user =models.OneToOneField(User, on_delete=models.CASCADE)
@@ -93,3 +92,12 @@ class Video(models.Model):
     def __str__(self):
         return self.title
 
+class Premium(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
+    stripe_subscription_id = models.CharField(max_length=255, blank=True, null=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Premium subscription for {self.user.username}"
