5c17c98054fbb7f179b046baf282179126cb1190
Ntsoaki <ntsoakisoai@gmail.com>
2025-12-01 09:43:39 +0200

    "settings.py changes"

--- newsapp_project/settings.py
+++ newsapp_project/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'sabuzz.premium_middleware.PremiumRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'newsapp_project.urls'
