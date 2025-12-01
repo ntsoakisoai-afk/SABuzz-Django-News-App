b119d92e6b78abdb979e3c345f05f0e410bc7b88
Ntsoaki <ntsoakisoai@gmail.com>
2025-12-01 07:12:08 +0200

    fixed admin.py

--- sabuzz/admin.py
+++ sabuzz/admin.py
@@ -11,17 +11,16 @@ admin.site.register(Profile, ProfileAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
-    list_filter = ('name',)
-    ordering = ('name',)
+    seacrh_fields = ('name',)
 
 admin.site.register(Category, CategoryAdmin)
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'status', 'created_at')
-    list_filter = ('status', 'created_at', 'author', 'category')
-    search_fields = ('title',)
-    date_hierarchy = 'created_at'
-    ordering = ('status', 'created_at')
+    list_filter = ('status', 'author', 'category')
+    search_fields = ('title', 'author__username')
+    ordering = ('created_at')
+    prepopulated_fields ={'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
 
@@ -47,13 +46,18 @@ admin.site.register(NewsletterSubscriber, NewsletterAdmin)
 class PremiumAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_active', 'subscribed_at')
     list_filter = ('is_active',)
+    search_fields = ('user__username', 'stripe_customer_id')
+
+admin.site.register(Premium, PremiumAdmin)
 
 class PodcastsAdmin(admin.ModelAdmin):
     list_display = ('title', 'uploaded_at', 'is_premium')
+    search_fields = ('title')
 
 admin.site.register(Podcasts, PodcastsAdmin)
 
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('title', 'uploaded_at', 'is_premium')
+    search_fields = ('title')
 
 admin.site.register(Video, VideoAdmin)
